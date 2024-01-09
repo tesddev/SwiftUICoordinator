c7a06d9acc4b8180ad1f79cf84b4bc90962e33ae
tesddev <tesleem.amuda@gmail.com>
2024-01-09 15:30:30 +0100

    update coordinator to use dependency injection

--- SwiftUICoordinator/Coordinator.swift
+++ SwiftUICoordinator/Coordinator.swift
@@ -7,27 +7,128 @@
 
 import SwiftUI
 
-enum Page: String, Identifiable {
+//enum Page: String, Identifiable {
+//    case apple, banana, carrot
+//
+//    var id: String {
+//        self.rawValue
+//    }
+//}
+//
+//enum Sheet: String, Identifiable {
+//    case lemon
+//
+//    var id: String {
+//        self.rawValue
+//    }
+//}
+//
+//enum FullScreenCover: String, Identifiable {
+//    case olive
+//
+//    var id: String {
+//        self.rawValue
+//    }
+//}
+//
+//
+//class Coordinator: ObservableObject {
+//    @Published var path = NavigationPath()
+//    @Published var sheet: Sheet?
+//    @Published var fullScreenCover: FullScreenCover?
+//
+//    func push(_ page: Page) {
+//        path.append(page)
+//    }
+//
+//    func present(_ sheet: Sheet) {
+//        self.sheet = sheet
+//    }
+//
+//    func present(fullScreenCover: FullScreenCover) {
+//        self.fullScreenCover = fullScreenCover
+//    }
+//
+//    func pop(){
+//        path.removeLast()
+//    }
+//
+//    func popToRoot(){
+//        path.removeLast(path.count)
+//    }
+//
+//    func dismissSheet(){
+//        self.sheet = nil
+//    }
+//
+//    func dismissFullScreenCover(){
+//        self.fullScreenCover = nil
+//    }
+//
+//    @ViewBuilder
+//    func build(page: Page) -> some View {
+//        switch page {
+//        case .apple:
+//            AppleView()
+//        case .banana:
+//            BananaView()
+//        case .carrot:
+//            CarrotView()
+//        }
+//    }
+//
+//    @ViewBuilder
+//    func build(sheet: Sheet) -> some View {
+//        switch sheet {
+//        case .lemon:
+//            NavigationStack {
+//                LemonView()
+//            }
+//        }
+//    }
+//
+//    @ViewBuilder
+//    func build(fullScreenCover: FullScreenCover) -> some View {
+//        switch fullScreenCover {
+//        case .olive:
+//            NavigationStack {
+//                OliveView()
+//            }
+//        }
+//    }
+//}
+
+enum Page: Hashable, Equatable, Identifiable {
     case apple, banana, carrot
+    case detail(profile: Profile)
     
     var id: String {
-        self.rawValue
+        switch self {
+        case .apple:
+            return "apple"
+        case .banana:
+            return "banana"
+        case .carrot:
+            return "carrot"
+        case .detail:
+            return "detail"
+        }
     }
 }
 
-enum Sheet: String, Identifiable {
+enum Sheet: Hashable, Equatable, Identifiable {
     case lemon
     
     var id: String {
-        self.rawValue
+        "lemon"
     }
 }
 
-enum FullScreenCover: String, Identifiable {
+enum FullScreenCover: Hashable, Equatable, Identifiable {
     case olive
     
     var id: String {
-        self.rawValue
+        "olive"
     }
 }
 
@@ -74,6 +175,8 @@ class Coordinator: ObservableObject {
             BananaView()
         case .carrot:
             CarrotView()
+        case .detail(let profile):
+            DetailView(profile: profile)
         }
     }
     
@@ -96,4 +199,8 @@ class Coordinator: ObservableObject {
             }
         }
     }
+}
+
+struct Profile: Hashable, Equatable {
+    let name: String
 }
